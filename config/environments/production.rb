3f86d315207678fec3891a94bf6f452b37245c1c
melc <yodhh@hotmail.com>
2017-09-21 22:19:28 -0700

    change config.assets.compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Gmap4Events::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
 
